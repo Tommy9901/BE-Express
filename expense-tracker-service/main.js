79e5356ead08cb6cab9070d8bd3b68064dee98a4
Tommy9901 <temuuujin0317@gmail.com>
2024-08-16 18:11:38 +0800

    dghdsg

--- expense-tracker-service/main.js
+++ expense-tracker-service/main.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require("fs");
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
 
-const categories = [
-  {
-    name: "Shopping",
-  }
-];
+const content = fs.readFileSync("categories.json", "utf-8");
+const categories = JSON.parse(content);
 
 app.get("/", (req, res) => {
   res.send("Hello Woooorld!");
 });
 
+// create
 app.get("/categories/list", (req, res) => {
   res.json(categories);
 });
-
 app.get("/categories/create", (req, res) => {
   const { name } = req.query;
-
   categories.push({ name: name });
+
+  fs.writeFileSync("categories.json", JSON.stringify(categories));
+
+  res.json(["Success"]);
+}); //end
+
+
+
+// update
+app.get("/categories/update", (req, res) => {
+  // value
   res.json(["Success"]);
 });
 
-// app.get("/", (req, res) => {
-//     res.send("Hello Woooorld!");
-//   });
+// delete
+app.get("/categories/delete", (req, res) => {
+  // value
+  res.json(["Success"]);
+});
+
+
+
+
 
 app.get("/articles", (req, res) => {
     // tootsoolol
--- expense-tracker-web/app/page.js
+++ expense-tracker-web/app/page.js
@@ -5,27 +5,33 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
+  const loadList = () => {
     fetch("http://localhost:4000/categories/list")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setCategories(data);
       });
+  }
+
+  useEffect(() => {
+   loadList();
   }, []);
 
   const createNew = () => {
     const name = prompt("Name...");
-    fetch(`http://localhost:4000/categories/list${name}`)
+    fetch(`http://localhost:4000/categories/create?name=${name}`)
       .then((res) => res.json())
-      .then((data) => {});
+      .then(() => {
+        loadList();
+      });
   }
 
   return(
     <main>
       <button onClick={createNew}>Add New</button>
      {categories.map((category) => (
-      <div key={category.name}>{category.name}</div>
+      <div key={category.name}>{category.name} <button>edit</button> <button>delete</button></div>
      ))}
     </main>
   );
